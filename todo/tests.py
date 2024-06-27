e58b22ec6e1f593be0a48037c17b36ff9162b9b8
1F10230204 <s1f102302045@iniad.org>
2024-06-27 13:36:14 +0900

    トップページを追加

--- todo/tests.py
+++ todo/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
+
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
